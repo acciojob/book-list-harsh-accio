0461f266a3054d10c7fb710700fdf0cdcf0c28e6
harsh-accio <rajputharsh9897@gmail.com>
2025-09-21 10:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let title='';
+let author='';
+let isbn='';
+
+const tbody = document.querySelector('#book-list');
+
+document.getElementById('submit').addEventListener('click',()=>{
+    title=document.getElementById('title').value.trim();
+    author=document.getElementById('author').value.trim();
+    isbn=document.getElementById('isbn').value.trim();
+    console.log(title,author,isbn);
+    
+
+    if(!author || !title || !isbn){
+        alert("Fields are Empty");
+        return;
+    }
+
+    const row = document.createElement('tr');
+    row.innerHTML=`
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><button class="btn btn-danger delete" >X</button></td>
+    `;
+    row.querySelector('.delete').onclick=()=>{
+        row.remove();
+    }
+    tbody.appendChild(row);
+})
